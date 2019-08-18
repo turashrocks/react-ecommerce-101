ab0a654a881125aee4e0af75c1f592b2747e08cd
Willkommen <turash@gmx.de>
2019-08-18 22:37:19 +0600

    second commit

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import PayPalButton from './PayPalButton'
 
-export default function CartTotals({value}) {
+
+export default function CartTotals({value, history}) {
     const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
     return (
         <>
@@ -17,6 +19,7 @@ export default function CartTotals({value}) {
                     <h5><span className="text-title">subtotal: </span><strong>৳{cartSubTotal}</strong></h5>
                     <h5><span className="text-title">tax: </span><strong>৳{cartTax}</strong></h5>
                     <h5><span className="text-title">total: </span><strong>৳{cartTotal}</strong></h5>
+                    <PayPalButton total={cartTotal} clearCart={clearCart} history={history}/>
                 </div>
             </div>
         </div>
--- src/components/Default.js
+++ src/components/Default.js
@@ -9,6 +9,7 @@ export default class Default extends Component {
                         <h1 className="display-3">404</h1>
                         <h1>error</h1>
                         <h2>page not found</h2>
+                        <h3>the requested URL<span className="text-danger">{this.props.location.pathname}</span>{""} was not found</h3>
                     </div>
                 </div>
             </div>
